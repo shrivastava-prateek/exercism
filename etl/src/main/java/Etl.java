e4b33403cc1874e729d39712a39e50c8336ab43b
prateek-shrivastava <prateek.shrivastava59@gmail.com>
2016-12-29 00:54:16 +0530

    added algo

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -3,6 +3,16 @@ import java.util.Map;
 
 public class Etl {
     public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+		//create a map same as return type
+		
+		//get the list of thr keys from map
+		
+		//for each on keys
+		
+		//retrieves the list and foreach every element
+		//put each entry on a map with same point value
+		
+		
         return null;
     }
 }
