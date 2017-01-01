f9c9de56b5f440d4937faa8649fb385a19422b42
prateek-shrivastava <prateek.shrivastava59@gmail.com>
2017-01-01 22:19:08 +0530

    implemented word-count

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -4,6 +4,22 @@ public class WordCount {
 
 	public Map<String, Integer> phrase(String string) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Map<String, Integer> countWords = new HashMap<String, Integer>();
+		String[] strArr= string.split(" ");
+		int len = strArr.length;
+		int count,i,j;
+		for (i=0;i<len;i++){
+			count=1;
+			if(countWords.containsKey(strArr[i])){
+			for (j=i;j<len;j++){
+				if (strArr[i].equalsIgnoreCase(strArr[j])){
+					count++;
+				}
+			}
+			countWords.put(strArr[i],count);
+			}
+		}
+		return countWords;
 	}
 }
