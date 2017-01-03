920e1f88d66daa0a18dbeb363e497be771eef49e
Prateek Shrivastava <prateek.shrivastava59@gmail.com>
2017-01-03 12:09:26 +0530

    added methods to anagram

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class Anagram {
+
+	public Anagram(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> match(List<String> asList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
